98ae04fe1b4bcf998392243f0515ecabe2b1a643
Vasu Vijayvargiya <56105511+Vasuvj24@users.noreply.github.com>
2021-11-21 22:40:43 +0530

    complete netflix clone with working trailer option with youtube extension of npm

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './App.css';
+import Row from './Row'
+import requests from './request';
+import Banner from './Banner'
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Banner trunccare="truncare-ooiaw-sali"></Banner>
+      {/* WE USED 1 COMPONENT AND MADE MANY BY JUST USING ONE COMPONENT OVER AND OVER AGAIN */}
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      {/* <Row title="Documentries" fetchUrl={requests.fetchDocumentries}/>  */}
+      {/* making a component and then reviving in this and  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Banner.js
@@ -0,0 +1,49 @@
+import React,{useState,useEffect} from 'react'
+import axios from './axios'
+import requests from './request'
+import './Banner.css'
+function Banner(props) {
+    console.log("inside banner");
+    const [movies,setMovies] = useState([]);
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            // console.log(request.data.results);
+            //code for getting a random movie
+            setMovies(request.data.results[Math.floor(Math.random() * request.data.results.length-1)]);
+        }
+        fetchData();
+        return fetchData();
+        }, []);
+        console.log(movies); 
+        // when you need dots after some character
+        function truncate(str,n){
+            return str?.length>n ? str.substr(0,n-1) + "..." : str;
+        }   
+        // console.log(movies.backdrop_path);
+    return (
+        //ab agar yha css likhne h templte literal k sath to aise likhte h or style k andar bhe ek object pass krna pdta h as hm style ko bhe as object likhte h
+        <div className="banner"
+        style={{
+            //yha agar movies h to uske backdrop path p jao . ka mtlb yhi h
+        backgroundImage:`url( "https://image.tmdb.org/t/p/original/${movies?.backdrop_path}")`,
+        backgroundSize:'cover',
+        backgroundPosition: 'center center'
+        }}
+        >
+            <div className="banner-contents">
+                <h1 className="banner-title">{movies?.title||  movies?.name || movies?.original_name}</h1>
+                <div className="banner-buttons">
+                    <button className="banner-button">Play</button>
+                    <button className="banner-button">My List</button>
+                </div>
+                <h1 className="banner-description">
+                    {truncate(movies?.overview,150)}
+                </h1>
+            </div>
+            <div className="fade-bottom"></div>
+        </div>
+    )
+}
+
+export default Banner
